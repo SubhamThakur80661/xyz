fe5bf4bdb630609f0f98bbd297b69d43c1d35162
subham <110287317+gpsnavigation@users.noreply.github.com>
2024-04-28 21:04:57 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,24 +1,41 @@
 <?php
 
-  $receiving_email_address = 'shubhamthakur80669@gmail.com';
+// Set the receiving email address
+$receiving_email_address = 'shubhamthakur80669@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Check if the PHP Email Form library exists
+if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+    include($php_email_form);
+} else {
+    die('Unable to load the "PHP Email Form" Library!');
+}
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// Create a new instance of PHP_Email_Form
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// Set the recipient's email address
+$contact->to = $receiving_email_address;
 
-  echo $contact->send();
+// Set sender's name, email, and subject from form inputs
+$contact->from_name = isset($_POST['name']) ? $_POST['name'] : '';
+$contact->from_email = isset($_POST['email']) ? $_POST['email'] : '';
+$contact->subject = isset($_POST['subject']) ? $_POST['subject'] : '';
+
+// Add message content from form inputs
+$contact->add_message(isset($_POST['name']) ? $_POST['name'] : '', 'From');
+$contact->add_message(isset($_POST['email']) ? $_POST['email'] : '', 'Email');
+$contact->add_message(isset($_POST['message']) ? $_POST['message'] : '', 'Message', 10);
+
+// Attempt to send the email
+$result = $contact->send();
+
+// Check if the email was sent successfully
+if ($result === true) {
+    // If successful, send a success message
+    echo 'OK';
+} else {
+    // If not successful, send an error message
+    echo 'Error: ' . $result;
+}
 ?>
